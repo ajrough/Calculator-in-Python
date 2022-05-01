887fe8f3c502486684657baa71e637d752de9a6c
Ajay Ram <ajayramsaravanan4@gmail.com>
2022-05-01 10:37:15 +0530

    Added Options and Mulltiplication Function

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 print("CALCULATOR")
-print("Enter 2 number:")
-a=int(input("Enter 1st number :"))
-b=int(input("Enter 2nd number :"))
-print("The sum is",a+b)
+print("1.Addition")
+print("2.Multiplication")
+opt=int(input("Enter your choice :"))
+if opt==1:
+    print("Enter 2 number:")
+    a=int(input("Enter 1st number :"))
+    b=int(input("Enter 2nd number :"))
+    print("The Sum is",a+b)
+elif opt==2:
+    print("Enter 2 number:")
+    a=int(input("Enter 1st number :"))
+    b=int(input("Enter 2nd number :"))
+    print("The Product is",a*b)
