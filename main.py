63273d03e91ec3b4f5855a468f7815039f4314c3
CampVamp <92621098+CampVamp@users.noreply.github.com>
2022-05-02 19:21:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 print("CALCULATOR")
 print("1.Addition")
-print("2.Multiplication")
+print("2.Subtraction")
+print("3.Multiplication")
+print("4.Division")
 opt=int(input("Enter your choice :"))
 if opt==1:
     print("Enter 2 number:")
@@ -11,4 +13,14 @@ elif opt==2:
     print("Enter 2 number:")
     a=int(input("Enter 1st number :"))
     b=int(input("Enter 2nd number :"))
+    print("The Product is",a-b)
+elif opt==3:
+    print("Enter 2 number:")
+    a=int(input("Enter 1st number :"))
+    b=int(input("Enter 2nd number :"))
     print("The Product is",a*b)
+elif opt==4:
+    print("Enter 2 number:")
+    a=int(input("Enter 1st number :"))
+    b=int(input("Enter 2nd number :"))
+    print("The Product is",a/b)
